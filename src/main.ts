0aeb83bdd116979e65f9c4ffc02f1f7824118112
jeon min ji <jeon320@naver.com>
2026-06-11 09:13:26 +0900

    fix: main.ts 에 CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
